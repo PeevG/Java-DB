8a4b913b5cebb43c0807da836d187de9b2a4ef21
PeevG <Peevgeorgip@gmail.com>
2021-06-08 22:45:58 +0300

    Homework added

---
+++ SubqueriesAndJoins/SubqueriesAndJoins-Lab/01.Managers.sql
@@ -0,0 +1,10 @@
+-- Subqueries and Joins 
+
+-- Lab 1
+SELECT e.`employee_id`, CONCAT(e.`first_name`, ' ', e.`last_name`) AS `full_name`,
+d.`department_id`, d.`name` AS `department_name`
+FROM `departments` AS d
+JOIN `employees` AS e
+ON d.`manager_id` = e.`employee_id`
+ORDER BY `employee_id`
+LIMIT 5;
