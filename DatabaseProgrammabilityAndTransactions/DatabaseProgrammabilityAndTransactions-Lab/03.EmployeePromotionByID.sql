5b87dddadfaecde33822300388c50f7cee2816e1
PeevG <Peevgeorgip@gmail.com>
2021-06-13 19:05:33 +0300

    Refactoring project

--- DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions-Lab/03.EmployeePromotionByID.sql
+++ DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions-Lab/03.EmployeePromotionByID.sql
@@ -1,8 +1,11 @@
 CREATE PROCEDURE `usp_raise_salary_by_id`(chosen_employee INT)
 BEGIN
 
+IF ((SELECT COUNT(*) FROM employees WHERE employee_id = chosen_employee) = 1 )
+THEN
 UPDATE employees
 SET salary = salary * 1.05
 WHERE employee_id = chosen_employee;
+END IF;
 
 END
