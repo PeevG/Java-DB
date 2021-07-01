497308b8653944eab30e3c86ed976bf487841d92
PeevG <Peevgeorgip@gmail.com>
2021-07-01 23:02:59 +0300

    Homework added

--- SpringData/Introduction-To-Hibernate-Exer/Skeleton/src/main/java/Engine.java
+++ SpringData/Introduction-To-Hibernate-Exer/Skeleton/src/main/java/Engine.java
@@ -1,11 +1,14 @@
+import entities.Employee;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.transaction.Transaction;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Engine implements Runnable{
+public class Engine implements Runnable {
 
     private final EntityManager entityManager;
     private BufferedReader bufferedReader;
@@ -25,10 +28,35 @@ public class Engine implements Runnable{
 
             switch (exerciseNumber) {
                 case 2 -> ExerciseTwoChangeCasing();
+                case 3 -> ExerciseThree3ContainsEmployee();
+                case 4 -> ExerciseFourEmployeeWithSalaryOver();
             }
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void ExerciseFourEmployeeWithSalaryOver() {
+
+    }
+
+    private void ExerciseThree3ContainsEmployee() throws IOException {
+        System.out.println("Please enter first and last name separated by space: ");
+
+        String[] input = bufferedReader.readLine().split("\\s+");
+
+        try {
+            Employee employee = entityManager.createQuery("SELECT e FROM Employee e " +
+                    "WHERE e.firstName = :first_name AND e.lastName = :last_name", Employee.class)
+                    .setParameter("first_name", input[0])
+                    .setParameter("last_name", input[1])
+                    .getSingleResult();
+
+            System.out.println("Yes");
+
+        } catch (NoResultException e) {
+            System.out.println("No");
         }
     }
 
