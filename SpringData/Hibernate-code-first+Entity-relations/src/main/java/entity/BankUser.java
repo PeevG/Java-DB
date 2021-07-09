dc4f2e4e72b7e4704b959b0983cdac328f4447ec
PeevG <Peevgeorgip@gmail.com>
2021-07-09 22:54:13 +0300

    New homework added

---
+++ SpringData/Hibernate-code-first+Entity-relations/src/main/java/entity/BankUser.java
@@ -0,0 +1,58 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class BankUser extends BaseEntity{
+
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public BankUser() {
+    }
+
+    @Column(name = "first_name", nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "password", nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
