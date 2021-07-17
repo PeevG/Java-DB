5b91ebb6be71cd3d35e55d6828136ceb7d723de8
PeevG <Peevgeorgip@gmail.com>
2021-07-17 20:30:59 +0300

    Homework added

---
+++ SpringData/SpringDataAdvancedQuering-Exer/Skeleton/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -0,0 +1,87 @@
+package com.example.springintro;
+
+import com.example.springintro.model.entity.AgeRestriction;
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.service.AuthorService;
+import com.example.springintro.service.BookService;
+import com.example.springintro.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+    private final AuthorService authorService;
+    private final BookService bookService;
+    private final BufferedReader bufferedReader;
+
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService, BufferedReader bufferedReader) {
+        this.categoryService = categoryService;
+        this.authorService = authorService;
+        this.bookService = bookService;
+        this.bufferedReader = bufferedReader;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedData();
+
+        // printAllBooksAfterYear(2000);
+        // printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(1990);
+        // printAllAuthorsAndNumberOfTheirBooks();
+        // printALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
+
+        System.out.println("Please enter exercise number: ");
+        int exerciseNumber = Integer.parseInt(bufferedReader.readLine());
+
+        switch (exerciseNumber) {
+            case 1 -> printAllBooksThatMatchesRequiredAgeRestriction();
+        }
+
+    }
+
+    private void printAllBooksThatMatchesRequiredAgeRestriction() throws IOException {
+        System.out.println("Please select age restriction: Minor, Teen or Adult.");
+
+        AgeRestriction ageRestriction = AgeRestriction.valueOf(bufferedReader.readLine().toUpperCase());
+
+        bookService.findAllBookTitlesWithAgeRestriction(ageRestriction)
+                .forEach(System.out::println);
+    }
+
+    private void printALlBooksByAuthorNameOrderByReleaseDate(String firstName, String lastName) {
+        bookService
+                .findAllBooksByAuthorFirstAndLastNameOrderByReleaseDate(firstName, lastName)
+                .forEach(System.out::println);
+    }
+
+    private void printAllAuthorsAndNumberOfTheirBooks() {
+        authorService
+                .getAllAuthorsOrderByCountOfTheirBooks()
+                .forEach(System.out::println);
+    }
+
+    private void printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(int year) {
+        bookService
+                .findAllAuthorsWithBooksWithReleaseDateBeforeYear(year)
+                .forEach(System.out::println);
+    }
+
+    private void printAllBooksAfterYear(int year) {
+        bookService
+                .findAllBooksAfterYear(year)
+                .stream()
+                .map(Book::getTitle)
+                .forEach(System.out::println);
+    }
+
+    private void seedData() throws IOException {
+        categoryService.seedCategories();
+        authorService.seedAuthors();
+        bookService.seedBooks();
+    }
+}
